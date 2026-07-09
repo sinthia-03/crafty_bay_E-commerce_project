9e61ae25d72fac87c6f6bf6db06f8b728322a4ed
sinthia-03 <sinthiaorita37@gmail.com>
2026-07-10 01:39:39 +0600

    post API

--- lib/core/service/network_caller/network_caller.dart
+++ lib/core/service/network_caller/network_caller.dart
@@ -3,63 +3,121 @@ import 'package:http/http.dart';
 import 'package:logger/logger.dart';
 
 class NetworkCaller {
-  final Logger _logger = Logger(
+  final Logger _logger = Logger();
 
-  );
+  late final Map<String, String> Function() headers;
+  NetworkCaller({required this.headers});
+
+  //get
   Future<NetworkResponse> getRequest(String url) async {
-    Uri uri = Uri.parse(url);
+    try {
+      Uri uri = Uri.parse(url);
 
-    _logRequest(url);
-    //url pass
-    final Response response = await get(uri);
+      _logRequest(url, headers: headers());
+      //url pass
+      final Response response = await get(uri, headers: headers());
 
-    _logResponse(response);
+      _logResponse(response);
 
-    if (response.statusCode == 200) {
-      //succees
-      final decodedJson = jsonDecode(response.body); // encoded to decode kora
+      if (response.statusCode == 200) {
+        //succees
+        final decodedJson = jsonDecode(response.body); // encoded to decode kora
+        return NetworkResponse(
+          isSuccess: true,
+          statusCode: response.statusCode,
+          body: decodedJson,
+        );
+      } else {
+        //failed{
+        //'Something went wrong!',
+        //}
+        final decodedJson = jsonDecode(response.body);
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+          errorMessage: decodedJson['message'] ?? 'Something went wrong!',
+        );
+      }
+    } on Exception catch (e) {
       return NetworkResponse(
         isSuccess: true,
-        statusCode: response.statusCode,
-        body: decodedJson,
-      );
-    } else {
-      //failed{
-      //'Something went wrong!',
-    //}
-      final decodedJson = jsonDecode(response.body);
-      return NetworkResponse(
-        isSuccess: false,
-        statusCode: response.statusCode,
-        errorMessage: decodedJson['message'] ?? 'Something went wrong!',
+        statusCode: -1,
+        errorMessage: e.toString(),
       );
     }
   }
 
-  void _logRequest(String url,{Map<String, dynamic>? requestBody, Map<String, String>? headers}){
- _logger.d('''URL=> $url
+  void _logRequest(
+    String url, {
+    Map<String, dynamic>? requestBody,
+    Map<String, String>? headers,
+  }) {
+    _logger.d('''URL=> $url
  Headers=> $headers
  RequestBody=>$requestBody''');
   }
 
-  void _logResponse(Response response){
-    if(response.statusCode == 200 || response.statusCode == 201) {
+  void _logResponse(Response response) {
+    if (response.statusCode == 200 || response.statusCode == 201) {
       _logger.i('''URL=> ${response.request?.url}
  Status Code=> ${response.statusCode}
  RequestBody=>${response.body}''');
-    }else{
+    } else {
       _logger.e('''URL=> ${response.request?.url}
  Status Code=> ${response.statusCode}
  RequestBody=>${response.body}''');
     }
-
   }
 
+  ////////////////////////////POST Method
+
+  Future<NetworkResponse> postRequest(
+    String url, {
+    Map<String, dynamic>? body,
+  }) async {
+    try {
+      Uri uri = Uri.parse(url);
+
+      _logRequest(url, requestBody: body, headers: headers());
+      //url pass
+      final Response response = await post(
+        uri,
+        headers: headers(),
+        body: jsonEncode(body),
+      );
+      _logResponse(response);
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        //succees
+        final decodedJson = jsonDecode(response.body); // encoded to decode kora
+        return NetworkResponse(
+          isSuccess: true,
+          statusCode: response.statusCode,
+          body: decodedJson,
+        );
+      } else {
+        //failed{
+        //'Something went wrong!',
+        //}
+        final decodedJson = jsonDecode(response.body);
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+          errorMessage: decodedJson['message'] ?? 'Something went wrong!',
+        );
+      }
+    } on Exception catch (e) {
+      return NetworkResponse(
+        isSuccess: false,
+        statusCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
 }
 
-
-
-class NetworkResponse {  // custom object
+class NetworkResponse {
+  // custom object
   final bool isSuccess;
   final int statusCode;
   final dynamic body;
@@ -68,7 +126,7 @@ class NetworkResponse {  // custom object
   NetworkResponse({
     required this.isSuccess,
     required this.statusCode,
-     this.body,
+    this.body,
     this.errorMessage,
   });
 }
